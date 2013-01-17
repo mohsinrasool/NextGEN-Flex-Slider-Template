2af157c856f50b1a09cca38a2bcb99ff49dc66a3
mohsinrasool <mohsinrasool@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-01-17 14:34:51 +0000

    Updated plugin URL
    git-svn-id: http://plugins.svn.wordpress.org/nextgen-flex-slider-template/trunk@654288 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- nextgen-flex-slider-template.php
+++ nextgen-flex-slider-template.php
@@ -1,10 +1,10 @@
 <?php
 /*
 Plugin Name: NextGEN Flex Slider Template
-Plugin URI: http://wordpress.org/extend/plugins/nextgen-flex-slider-template/
+Plugin URI: http://mohsinrasool.wordpress.com/2012/12/21/nextgen-flex-slider-template/
 Description: Add a "sliderview" template for the NextGen gallery. Use the shortcode [nggallery id=x template="sliderview"] to display images as the slider.
 Author: Mohsin Rasool
-Author URI: http://mohsinrasool.wordpress.com/nextgen-flex-slider-template
+Author URI: http://mohsinrasool.wordpress.com
 Version: 1.1
 */
 
--- readme.txt
+++ readme.txt
@@ -1,6 +1,6 @@
 === NextGEN Flex Slider Template ===
 Contributors: mohsinrasool
-Donate link: http://mohsinrasool.wordpress.com/nextgen-flex-slider-template
+Donate link: http://mohsinrasool.wordpress.com/2012/12/21/nextgen-flex-slider-template/
 Tags: nextgen, gallery, slider, slideshow, nextgen-gallery, template, flexslider, nextgen slider, image slider, content slider, nextgen image slider, nextgen content slider, featured slider, nextgen featured slider
 Requires at least: 3.0.1
 Tested up to: 3.5
